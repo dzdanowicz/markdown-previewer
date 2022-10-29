9ed12e274ebc1cdacd47b2a87a06eecbfdaaf601
dzdanowicz <contact@dzdanowicz.com>
2022-10-29 13:10:02 +0200

    Files cleanup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./App/App.css";
 import App from "./App/App.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
